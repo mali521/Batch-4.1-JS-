8a9e4e9654fd68f192ea4d73a15e4fdfd90e4d3d
Muhammad Ali Mughal <mamughal007@gmail.com>
2017-02-26 19:12:38 +0500

    1st Class

---
+++ 1st Class (26-2-2017)/app.js
@@ -0,0 +1,16 @@
+// alert("Hello World");
+
+// var a = 10;
+// var b = 15;
+// // var c = a + b;
+// alert("Hello World"+ (a+b)); 
+
+// var a = "10";
+// var b = 15;
+// var c = a + b;
+// alert(c); 
+
+var a = 10;
+var a = "ali";
+var b= a;
+alert(a+b);
